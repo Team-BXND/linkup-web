b8ee2f37450c347640aa77f02ccfedd99646d650
lee090626 <hyeongseogi46@gmail.com>
2026-01-13 20:49:47 +0900

    fix: 사용하지 않는 import 제거

--- src/components/PoularHot/index.tsx
+++ src/components/PoularHot/index.tsx
@@ -1,5 +1,4 @@
 import * as S from './style';
-import { popularHot } from '@/constants/popularHot.constants';
 import { Title } from '../Text';
 import QuestionItem from '../QuestionItem';
 import { publicAxios } from '@/libs/customAxios';
