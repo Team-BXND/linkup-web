1a0d333477bba45b69156cf352d7be77423d1413
jdw09 <razion0304@naver.com>
2026-01-13 14:36:28 +0900

    refactor: 로그인 훅 리디렉션 제거

--- src/hooks/Auth/useLogin.ts
+++ src/hooks/Auth/useLogin.ts
@@ -1,7 +1,6 @@
 import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/constants/token.constants"
 import { cookie } from "@/utils/cookie"
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 /* refreshToken 유효기간 : 7일 */
 const REFRESH_TOKEN_LIFETIME = 168;
@@ -9,13 +8,10 @@ const REFRESH_TOKEN_LIFETIME = 168;
 const ACCESS_TOKEN_LIFETIME = 0.5;
 
 export const useLogin = (accessToken: string, refreshToken: string) => {
-  const navigate = useNavigate();
-  
   useEffect(() => {
     if (accessToken && refreshToken) {
       cookie.set(ACCESS_TOKEN_KEY, accessToken, ACCESS_TOKEN_LIFETIME);
       cookie.set(REFRESH_TOKEN_KEY, refreshToken, REFRESH_TOKEN_LIFETIME);
     };
-    navigate("/");
   });
 };
