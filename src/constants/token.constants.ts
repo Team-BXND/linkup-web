4697c5d5c1242e0918bb62c7646db0f46725771f
jdw09 <razion0304@naver.com>
2026-01-13 15:31:55 +0900

    refactor: 토큰 유효기간 상수 선언

--- src/constants/token.constants.ts
+++ src/constants/token.constants.ts
@@ -1,2 +1,7 @@
 export const ACCESS_TOKEN_KEY = "accessToken";
 export const REFRESH_TOKEN_KEY = "refreshToken";
+
+/* refreshToken 유효기간 : 7일 */
+export const REFRESH_TOKEN_LIFETIME = 168;
+/* accessToken 유효기간 : 30분 */
+export const ACCESS_TOKEN_LIFETIME = 0.5;
--- src/hooks/Auth/useLogin.ts
+++ src/hooks/Auth/useLogin.ts
@@ -1,12 +1,7 @@
-import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "@/constants/token.constants"
+import { ACCESS_TOKEN_KEY, ACCESS_TOKEN_LIFETIME, REFRESH_TOKEN_KEY, REFRESH_TOKEN_LIFETIME } from "@/constants/token.constants"
 import { cookie } from "@/utils/cookie"
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
-/* refreshToken 유효기간 : 7일 */
-const REFRESH_TOKEN_LIFETIME = 168;
-/* accessToken 유효기간 : 30분 */
-const ACCESS_TOKEN_LIFETIME = 0.5;
 
 export const useLogin = (accessToken: string, refreshToken: string) => {
   const navigate = useNavigate();
