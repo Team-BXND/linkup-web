fd85a35bdd24b893dc2948e5789cde0aff4b44ac
lee090626 <hyeongseogi46@gmail.com>
2026-01-13 20:37:10 +0900

    feat: Home 페이지 인기질문리스트 axios 작성

--- src/components/PoularHot/index.tsx
+++ src/components/PoularHot/index.tsx
@@ -2,15 +2,47 @@ import * as S from './style';
 import { popularHot } from '@/constants/popularHot.constants';
 import { Title } from '../Text';
 import QuestionItem from '../QuestionItem';
+import { publicAxios } from '@/libs/customAxios';
+import { useState, useEffect } from 'react';
+import type { PopularHotResponse } from '@/types/popularHot';
+
+const SERVER_URL = import.meta.env.VITE_SERVER_URL
 
 function PopularHot() {
+  const [popularHotData, setPopularHotData] = useState<PopularHotResponse>({
+    data: [],
+    meta: {
+      total: 0,
+      page: 1,
+      pageSize: 0,
+      totalPages: 0,
+      hasNext: false,
+      hasPrevious: false,
+    },
+  });
+
+  useEffect(() => {
+    publicAxios
+      .get(`${SERVER_URL}/popular/hot`, {
+        params: {
+          page: 5,
+        },
+      })
+      .then((response) => {
+        setPopularHotData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <S.Container>
       <Title size="md" weight="bold">
         🔥 지금 뜨거운 Q&A
       </Title>
       <S.QuestionsList>
-        {popularHot.data.map((item, key) => (
+        {popularHotData.data.map((item, key) => (
           <QuestionItem item={item} index={key} showRank={true}></QuestionItem>
         ))}
       </S.QuestionsList>
