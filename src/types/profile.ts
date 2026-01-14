8084445f77df6b81748238d3dab9f71b49b827df
황경민 <aa01046044029@gmail.com>
2026-01-14 16:24:47 +0900

    feat:type 지정

---
+++ src/types/profile.ts
@@ -0,0 +1,36 @@
+type ProfileCategory = 'all' | 'code' | 'school' | 'project';
+
+export interface ProfileMyinfo {
+    username:string;
+    email:string;
+    point:number;
+    rank:number;
+}
+
+export interface ProfileMyAnswer {
+    id:number;
+    title:string;
+    preview:string;
+    category:ProfileCategory;
+    answer:string;
+}
+
+export interface ProfileMyQuestion {
+    id:number;
+    title:string;
+    preview:string;
+    category:ProfileCategory;
+    like:number;
+    commentCount:number;
+    isAccepted:boolean;
+    page:number;
+}
+
+export interface ProfileMeta {
+    total:number;
+    page:number;
+    pageSize:number;
+    totalPages:number;
+    hasNext:boolean;
+    hasPrevious:boolean;
+}
