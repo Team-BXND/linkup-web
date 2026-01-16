2c82af8a0570db30f4f3be68a42057cf23962d1f
황경민 <aa01046044029@gmail.com>
2026-01-16 23:10:33 +0900

    refactor: 코드 띄워쓰기 정리

--- src/components/Category/style.ts
+++ src/components/Category/style.ts
@@ -1,14 +1,13 @@
 import styled from "styled-components";
 
-export const Category=styled.div`
+export const Category = styled.div`
+  padding: 4px 12px;
 
-    padding:4px 12px;
+  background-color: ${({ theme }) => theme.color.main.mainColor + "1A"};
+  border-radius: inherit;
 
-    background-color:${({ theme })=>theme.color.main.mainColor+"1A"};
-    border-radius: inherit;
-
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    color:${({ theme })=>theme.color.main.mainColor};
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.color.main.mainColor};
+`;
