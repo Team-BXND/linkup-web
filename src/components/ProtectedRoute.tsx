b969e5385e8eb0786d69a32c0ef90b9f95682399
황경민 <aa01046044029@gmail.com>
2026-01-23 18:44:50 +0900

    fix:merge conflict 수정

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,10 +3,10 @@ import { cookie } from "@/utils/cookie";
 import { Navigate, Outlet } from "react-router-dom";
 
 function ProtectedRoute() {
-  // const accessToken = cookie.get(ACCESS_TOKEN_KEY);
-  // if (!accessToken) {
-  //   return <Navigate to="/login" replace />;
-  // }
+  const accessToken = cookie.get(ACCESS_TOKEN_KEY);
+  if (!accessToken) {
+    return <Navigate to="/login" replace />;
+  }
 
   return <Outlet />;
 }
