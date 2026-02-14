b5c3d4bd64b0343f865407eb944c488f3b26ef05
jdw09 <razion0304@naver.com>
2026-02-14 23:07:35 +0900

    feat: 답변달기 API

--- src/components/WriteAnswer/index.tsx
+++ src/components/WriteAnswer/index.tsx
@@ -1,15 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import TextEditor from "../common/TextEditor";
 import * as S from "./style";
+import { linkupAxios } from "@/libs/customAxios";
 
-function WriteAnswer() {
+function WriteAnswer({ id }: { id?: string }) {
   const [content, setContent] = useState("");
-  const onSubmit = () => {};
-
-  useEffect(() => {
-    console.log(content);
-  }, [content]);
-
+  const onSubmit = (markdown: string) => {
+    linkupAxios
+      .post(`/posts/${id}/answer`, {
+        content: markdown,
+      })
+      .then(() => {
+        alert("댓글 등록이 완료되었습니다.");
+        location.reload();
+      })
+      .catch(() => {
+        alert("댓글을 작성하지 못했습니다. 잠시 후 다시 시도해주세요.");
+      });
+  };
   return (
     <S.Container>
       <TextEditor
