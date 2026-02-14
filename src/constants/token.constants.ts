975016f99d302f719b914236aec64e20c229ac5d
jdw09 <razion0304@naver.com>
2026-02-15 01:00:23 +0900

    fix: 토큰 유효기간

--- src/constants/token.constants.ts
+++ src/constants/token.constants.ts
@@ -4,4 +4,4 @@ export const REFRESH_TOKEN_KEY = "refreshToken";
 /* refreshToken 유효기간 : 7일 */
 export const REFRESH_TOKEN_LIFETIME = 168;
 /* accessToken 유효기간 : 30분 */
-export const ACCESS_TOKEN_LIFETIME = 0.01;
+export const ACCESS_TOKEN_LIFETIME = 0.5;
